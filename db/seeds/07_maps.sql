275cc9db522c384c15e63ec4fba5ffa9a45ec9e4
Baila3 <bailaly16@gmail.com>
2022-05-07 13:26:27 -0400

    add maps table to sql file

---
+++ db/seeds/07_maps.sql
@@ -0,0 +1,9 @@
+DROP TABLE maps CASCADE;
+CREATE table maps (
+  id  SERIAL PRIMARY KEY,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255),
+  initial_lat FLOAT,
+  initial_long FLOAT,
+  description TEXT
+)
