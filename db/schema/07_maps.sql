ce7e4b38d4bdfb753290dcd447c749bda256db44
Baila3 <bailaly16@gmail.com>
2022-05-07 14:10:58 -0400

    add NOT NULL

--- db/schema/07_maps.sql
+++ db/schema/07_maps.sql
@@ -1,9 +1,9 @@
 DROP TABLE maps CASCADE;
 CREATE table maps (
-  id  SERIAL PRIMARY KEY,
+  id  SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users2(id) ON DELETE CASCADE,
-  title VARCHAR(255),
-  initial_lat FLOAT,
-  initial_long FLOAT,
+  title VARCHAR(255) NOT NULL,
+  initial_lat FLOAT NOT NULL,
+  initial_long FLOAT NOT NULL,
   description TEXT
 );
