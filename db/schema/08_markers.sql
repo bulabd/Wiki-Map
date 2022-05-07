0839f4c02f10a2ccac24424ae04ce5edae41c5d9
bulabd <bulabd@outlook.com>
2022-05-07 18:23:02 +0000

    Create markers table

---
+++ db/schema/08_markers.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS users2 CASCADE;
+CREATE TABLE markers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  map_id INTEGER REFERENCES maps(id) ON DELETE CASCADE,
+  owner_id INTEGER REFERENCES users2(id),
+  title VARCHAR(255) NOT NUll,
+  description TEXT NOT NULL,
+  image_url VARCHAR(255),
+  latitude FLOAT NOT NULL,
+  longitude FLOAT NOT NULL,
+  type VARCHAR(255) NOT NULL
+);
