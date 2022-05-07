e917c77fafe335d9d2e6694072407312f4a2f42f
bulabd <bulabd@outlook.com>
2022-05-07 20:25:24 +0000

    Add users2 table seed

---
+++ db/seeds/06_users_seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO users2 (name, email, password)
+VALUES
+('Bulat Abdullin', 'bulat@mail.com', 'password'),
+('Baila Ly', 'baila@mail.com', 'password'),
+('George Burt', 'george@mail.com', 'password');
