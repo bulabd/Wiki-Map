4874bcaaa7ee90a4f21f4293225ab5d8b74e6b0a
Baila3 <bailaly16@gmail.com>
2022-05-07 16:11:55 -0400

    remove typos sql files

--- db/schema/09_users_favoutite_maps.sql
+++ db/schema/09_users_favoutite_maps.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS users_favoutite_maps CASCADE;
-CREATE table users_favoutite_maps (
+DROP TABLE IF EXISTS users_favourite_maps CASCADE;
+CREATE table users_favourite_maps (
   id  SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users2(id) ON DELETE CASCADE,
   map_id INTEGER REFERENCES maps(id) ON DELETE CASCADE,
