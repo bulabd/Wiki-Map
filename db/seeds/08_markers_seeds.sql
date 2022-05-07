569e9ea4900a89b8630e50afac78cf401e7d22ae
bulabd <bulabd@outlook.com>
2022-05-07 20:58:14 +0000

    Add markers seeds

---
+++ db/seeds/08_markers_seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO markers (map_id, owner_id, title, description, image_url, latitude, longitude, type)
+VALUES
+(1, 1, 'Marker 1', 'This is marker 1', 'https://media-cdn.tripadvisor.com/media/photo-s/19/68/9d/2d/fachada-da-nossa-pizzeria.jpg', 28.36657415570968, -81.31177204817727, 'pizza'),
+(1, 1, 'Marker 2', 'This is marker 2', 'https://choosecornwall.ca/wp-content/uploads/2021/01/Rudy-s-Pizzaria-Cornwall.jpg', 28.363924967983433, -81.36731162988244, 'pizza'),
+(2, 2, 'Marker 3', 'This is marker 3', 'https://emilypost.com/client_media/images/blogs/everyday-gym.jpg', 40.116576335923234, -88.23762511773795, 'gym'),
+(2, 2, 'Marker 4', 'This is marker 4', 'https://media.timeout.com/images/105619394/750/562/image.jpg', 40.11205009146218, -88.23660140403437, 'gym'),
+(3, 3, 'Marker 5', 'This is marker 5', 'https://media.blogto.com/uploads/2018/04/06/20141127-gooddayhairshop2048-04.jpg?cmd=resize&quality=70&w=1400&height=2500', 38.90216495328616, -77.0094387437626, 'haircut'),
+(3, 3, 'Marker 6', 'This is marker 6', 'https://media.blogto.com/articles/20201127-salons.jpg?w=2048&cmd=resize_then_crop&height=1365&quality=70', 38.900659816896116, -77.0168279991466, 'haircut');
