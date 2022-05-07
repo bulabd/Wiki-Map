f64e16973f60355635a5914e212bb4e043fea21d
Baila3 <bailaly16@gmail.com>
2022-05-07 16:04:29 -0400

    add register route skeleton

---
+++ routes/register.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    const userId = req.session.user_id
+    if (userId) {
+    return res.redirect("/user_maps/:id")
+    }
+
+  const templateVars =  { "sql": "querry" };
+  res.render('register_ejs', templateVars)
+  })
+
+
+
+
+router.post("/", (req, res) => {
+  const mail = req.body.email
+  const pass = req.body.password
+  if (!mail || !pass) {
+    return res.send(400)
+  }
+  // const userId = generateRandomString()
+  // const user = {id: userId, email: mail, password:  bcrypt.hashSync(pass, salt)}
+  const findEmail = findUserEmail("sql")
+  if ("sql") {
+    return  res.send(400)
+   }
+   users[userId] = user
+   const ID = userId
+  req.session.user_id = ID
+  res.redirect("/urls")
+  })
+}
