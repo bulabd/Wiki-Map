a3ace4bf4f50271640b251fb5935bd9b4314cba8
Baila3 <bailaly16@gmail.com>
2022-05-07 14:07:48 -0400

    create maps table

--- db/schema/07_maps.sql
+++ db/schema/07_maps.sql
@@ -1,7 +1,7 @@
 DROP TABLE maps CASCADE;
 CREATE table maps (
   id  SERIAL PRIMARY KEY,
-  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  owner_id INTEGER REFERENCES users2(id) ON DELETE CASCADE,
   title VARCHAR(255),
   initial_lat FLOAT,
   initial_long FLOAT,
