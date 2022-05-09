d2b531d0fae741914d87c1f9b9c4039410754435
Baila3 <bailaly16@gmail.com>
2022-05-09 15:19:15 -0400

    add querry for markers

--- routes/mapView.js
+++ routes/mapView.js
@@ -8,7 +8,7 @@ module.exports = (db) => {
       id: req.session.user_id
     };
     let query1 = `SELECT * FROM maps WHERE id = ${req.params.id}`;
-    console.log(req.params, 'reqparams');
+    // console.log(req.params, 'reqparams');
     db.query(query1)
       .then(data => {
         templateVars.map = {};
@@ -16,8 +16,14 @@ module.exports = (db) => {
         templateVars.map.initial_lat = data.rows[0].initial_lat;
         templateVars.map.initial_long = data.rows[0].initial_long;
         templateVars.map.description = data.rows[0].description;
-        console.log(templateVars);
-        res.render("mapView", templateVars);
+        // console.log(templateVars);
+        let query2 = `SELECT * FROM markers WHERE map_id = ${req.params.id}`;
+        db.query(query2)
+        .then(data2 => {
+          templateVars.markers = {};
+          for (const items of data2)
+          res.render("mapView", templateVars);
+        })
       });
   });
 
