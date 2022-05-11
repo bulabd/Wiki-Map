b5f242147484ec40fdc75a7048480cba0393004e
George-was-here <george.burt@gmail.com>
2022-05-11 20:57:20 +0000

    Premerge commit

--- routes/createMaps.js
+++ routes/createMaps.js
@@ -5,10 +5,14 @@ const request = require('request-promise-native');
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    // const templateVars = {
-    //   id: req.session.user_id
-    // };
-    res.render("createMaps");
+    const templateVars = {
+      id: req.session.user_id
+    };
+    if (templateVars.id) {
+      res.render("createMaps");
+    } else {
+      res.redirect("login");
+    }
   });
 
   router.post("/", (req, res) => {
@@ -17,30 +21,28 @@ module.exports = (db) => {
     };
     let { latitude, longitude } = 0;
     request('https://api.ipify.org?format=json', (error, response, body) => {
-     request(`https://freegeoip.app/json/${JSON.parse(body).ip}`, (error, response, body) => {
-        latitude =  JSON.parse(body).latitude;
-        longitude =  JSON.parse(body).longitude;
-        let values = [templateVars.id, req.body.title, latitude, longitude, req.body.description];
-        let query = `INSERT INTO maps (owner_id, title, initial_lat, initial_long, description) VALUES ($1, $2, $3, $4, $5) RETURNING *;`;
-        db.query(query, values)
-        .then(data => {
-          res.redirect(`/maps`);
-          return data;
+      if (error) {
+        res.redirect('/create');
+        alert('Something went wrong. Please try again.');
+      } else {
+        request(`https://freegeoip.app/json/${JSON.parse(body).ip}`, (error, response, body) => {
+          if (error) {
+            res.redirect('/create');
+            alert('Something went wrong. Please try again.');
+          } else {
+            latitude =  JSON.parse(body).latitude;
+            longitude =  JSON.parse(body).longitude;
+            let values = [templateVars.id, req.body.title, latitude, longitude, req.body.description];
+            let query = `INSERT INTO maps (owner_id, title, initial_lat, initial_long, description) VALUES ($1, $2, $3, $4, $5) RETURNING *;`;
+            db.query(query, values)
+            .then(data => {
+              res.redirect(`/maps`);
+              return data;
+            });
+          }
         });
-      });
+      }
     })
-
-
-    // .then( data => {
-    //   let values = [templateVars.id, req.body.title, data.latitude, data.longitude, req.body.description];
-    //   let query = `INSERT INTO maps (owner_id, title, initial_lat, initial_long, description) VALUES ($1, $2, $3, $4, $5) RETURNING *;`;
-    //   db.query(query, values)
-    //   .then(data => {
-    //     res.redirect(`/maps/${data.row[0].id}/edit`);
-    //   });
-    //  }
-    // )
   });
-
   return router;
 }
